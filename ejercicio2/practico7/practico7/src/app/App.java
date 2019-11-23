29a7404be3f9caab97279e132a297f121f39eebf
Jorge <jorgeluisgarciajlg@gmail.com>
2019-11-23 12:02:33 -0300

    modificacion practico 7

--- ejercicio2/practico7/practico7/src/app/App.java
+++ ejercicio2/practico7/practico7/src/app/App.java
@@ -1,71 +1,51 @@
 package app;
 
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-    //    Una casa de venta de electrónica, ha vendido en promedio en los últimos 4 meses, 200 capacitores, 
-    //    50 leds de color amarillo, 230 botones relé y 65 parlantes. El precio unitario de cada capacitor es de $135, 
-    //    el de cada led color amarillo es de $25, cada botón es de $187 y cada parlante cuesta $380.
-    //    a. Calcular el ingreso mensual de la casa de electrónica.
-    //    b. Calcular el ingreso para ventas que alcancen el 50%, 120% y 350% de las ventas señaladas.
-    //    c. Calcular para cada una de dichas hipótesis de venta, el stock de productos necesarios para cubrir 30, 45 y 
-    //    90 días de venta.
-        double cantCapacitores=200;
-        double cantLedAmarillo=50;
-        double cantRele=230;
-        double cantParlantes=65;
-        double precioCapacitores=135;
-        double precioLedAmarillo=25;
-        double preciotRele=187;
-        double precioParlantes=380;
-        double ingresoMensual=(cantCapacitores/4*precioCapacitores+cantLedAmarillo/4*precioLedAmarillo+cantParlantes/4*precioParlantes+cantRele/4*preciotRele);
-        System.out.println("Ejecicio 1 \na)");
+    public static void main(String[] args) {
+    
+        double cantVarillas=100;
+        double cantTuercas=200;
+        double cantLijas=50;
+        double cantFlexibles=40;
+        double precioVarillas=135;
+        double precioTuercas=25;
+        double precioLijas=187;
+        double precioFlexibles=380;
+        double ingresoMensual=(cantVarillas*precioVarillas+cantTuercas*precioTuercas+cantLijas*precioLijas+cantFlexibles*precioFlexibles)/3;
+        System.out.println("Ejecicio 1)");
         System.out.println("el ingreso mensual es de "+ingresoMensual);
-        System.out.println("b)");
-        ingresoMensual=(cantCapacitores/8*precioCapacitores+cantLedAmarillo/8*precioLedAmarillo+cantParlantes/8*precioParlantes+cantRele/8*preciotRele);
-        System.out.println("el ingreso mensual al 50% es de "+ingresoMensual);
-        ingresoMensual=(cantCapacitores/4*precioCapacitores*6/5+cantLedAmarillo/4*precioLedAmarillo*6/5+cantParlantes/4*precioParlantes*6/5+cantRele/4*preciotRele*6/5);
-        System.out.println("el ingreso mensual al 120% es de "+ingresoMensual);
-        ingresoMensual=(cantCapacitores/4*precioCapacitores*7/2+cantLedAmarillo/4*precioLedAmarillo*7/2+cantParlantes/4*precioParlantes*7/2+cantRele/4*preciotRele*7/2);
-        System.out.println("el ingreso mensual al 350% es de "+ingresoMensual);
-        System.out.println("d)");
-        hipotecisDeVenta(100, 30);
-        hipotecisDeVenta(100, 45);
-        hipotecisDeVenta(100, 90);
-        hipotecisDeVenta(50, 30);
-        hipotecisDeVenta(50, 45);
-        hipotecisDeVenta(50, 90);
-        hipotecisDeVenta(120, 30);
-        hipotecisDeVenta(120, 45);
-        hipotecisDeVenta(120, 90);
-        hipotecisDeVenta(350, 30);
-        hipotecisDeVenta(350, 45);
-        hipotecisDeVenta(350, 90);
-        AdmistradorDeItems.imprimirIngreso(10, 50);
-        AdmistradorDeItems.imprimirIngreso(10, 100);
-        AdmistradorDeItems.imprimirIngreso(10, 120);
-        AdmistradorDeItems.imprimirIngreso(10, 350);
-        int[] porcentages={50,100,120,350};
-        int[] dias={30,45,90};
+        System.out.println("Ejecicio 2)");
+        int[] meses={1,2,3};
+        int[] porcentages={50,90,100,110,120};
         for (int i = 0; i < porcentages.length; i++) {
-            
-            for (int j = 0; j < dias.length; j++) {
-                AdmistradorDeItems.imprimirStokParaRepocicion(dias[j], porcentages[i]);
+            for (int j = 0; j < meses.length; j++) {
+                ingresoParaVentas(porcentages[i],meses[j], ingresoMensual );
             }
         }
-    
+        System.out.println("Ejecicio 3)");
+        
+        int[] dias={30,45,90};
+        String[] items={"varilla","tuercas","flexible","lijas"};
+        double[]cant={cantVarillas,cantTuercas,cantLijas,cantFlexibles};
+        for (int i = 0; i < porcentages.length; i++) {
+            for (int j = 0; j < dias.length; j++) {
+                for (int j2 = 0; j2 < items.length; j2++) {
+                    stockNesesario(porcentages[i], dias[j], items[j2], cant[j2]); 
+                }
+            }
+        }
     }
-    public static void hipotecisDeVenta(int porcentage,int dias) {
-        double cantCapacitores=200;
-        double cantLedAmarillo=50;
-        double cantRele=230;
-        double cantParlantes=65;
-        double stockNecesarioCapacitores=cantCapacitores/4/30*dias*porcentage/100;
-        double stockNecesarioLed=cantLedAmarillo/4/30*dias*porcentage/100;
-        double stockNesesarioParlantes=cantParlantes/4/30*dias*porcentage/100;
-        double stockNecesarioRele=cantRele/4/30*dias*porcentage/100;
-        System.out.println("Stock necesario al "+porcentage+"% de capacitores para "+dias+" dias es de: "+stockNecesarioCapacitores);
-        System.out.println("Stock necesario al "+porcentage+"% de led para "+dias+" dias es de: "+stockNecesarioLed);
-        System.out.println("Stock necesario al "+porcentage+"% de parlantes para "+dias+" dias es de: "+stockNecesarioRele);
-        System.out.println("Stock necesario al "+porcentage+"% de rele para "+dias+" dias es de: "+stockNesesarioParlantes);
+  
+    public static void stockNesesario(int porcentage,int dias,String item,double cant) {
+        double stockNecesario=cant/3/30*dias*porcentage/100;
+        System.out.println("El stock necesario al "+porcentage+"% de ventas, de "+item+" para "+dias+" dias es de: "+stockNecesario);
+        
     }
-}
+    public static void ingresoParaVentas(int porcentage,int meses,double ingresoMensual ) {
+        double ingresoParaVentas=ingresoMensual*porcentage/100*meses;
+        System.out.println("Los ingresos al "+porcentage+"% son de: "+ingresoParaVentas+" hasta el mes "+meses);
+        
+    }
+ }
